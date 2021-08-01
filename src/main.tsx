5d73f376a8331da839d10047717ac5e4b0425c21
LittleC <i@ltlec.cn>
2021-08-01 20:39:51 +0800

    :zap: use react

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import './style.css'
 // @ts-ignore
 import _data from './data.csv'
 import '@amap/amap-jsapi-types'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const colors = ['#f44336', '#9c27b0', '#3f51b5', '#2196f3', '#009688', '#4caf50', '#ffeb3b', '#ff9800', '#795548', '#607d8b', '#ff5722', '#8bc34a']
 
@@ -16,8 +18,6 @@ const names = ["XX-潇湘赛", "SD-山东赛", "WZ-温州赛", "CQ-重庆赛", "
 
 // 不会
 // type Test<T extends string> = T extends `${infer S}-${infer N}` ? [S,N] : unknown
-
-let checked = names.map(v => v.split('-')[0])
 
 const markers: Record<string, AMap.LabelMarker[]> = {}
 
@@ -46,24 +46,17 @@ window.amapLoaded = function () {
   const infoWindow = new AMap.InfoWindow({
     anchor: 'top-left'
   })
-  const massMarks = new AMap.MassMarks([], {
-    zIndex: 1000,
-    zooms: [3, 16],
-    // @ts-ignore
-    style: colors.map(v => COLOR_POS(v)).map(v => ({
-      url: `data:image/svg+xml;base64,${btoa(v)}`,
-      size: new AMap.Size(11, 11),
-      anchor: new AMap.Pixel(5, 5)
-    }))
-  })
+  let massMarks: Record<string, AMap.MassMarks> = {}
+
+  let massItems: Record<string, AMap.MassData[]> = {}
+
   const layer = new AMap.LabelsLayer({
     zooms: [17, 20],
     zIndex: 1000,
     collision: false
   })
-  let massItems: AMap.MassData[] = []
   for (const item of data) {
-    let s = item.season.split("-")[0].match(/([A-Z]+)[0-9]+/)?.[1]
+    let s = item.season.split("-")[0].match(/([A-Z]+)[0-9]+/)?.[1] as string
     const marker = new AMap.LabelMarker({
       name: item.name,
       position: [item.GDlng, item.Gdlat],
@@ -83,7 +76,8 @@ window.amapLoaded = function () {
         }
       }
     })
-    massItems.push({
+    massItems[s] ||= []
+    massItems[s].push({
       lnglat: new AMap.LngLat(item.GDlng, item.Gdlat),
       style: item.sort % colors.length
     })
@@ -110,33 +104,57 @@ window.amapLoaded = function () {
     layer.add(marker)
   }
   map.add(layer)
-  massMarks.setData(massItems)
-  massMarks.setMap(map)
-  // @ts-ignore
-  document.getElementById("root")?.insertAdjacentHTML('beforeend', '<ul>' + names.map(v => `
-  <li>
-  <label>
-    <span>${v}</span>
-    <input type="checkbox" checked name=${v.split("-")[0]}>
-  </label>
-</li>
-`).join('') + '</ul>');
+  Object.keys(massItems).forEach(k => {
+    massMarks[k] = new AMap.MassMarks(massItems[k], {
+      zIndex: 1000,
+      zooms: [3, 16],
+      // @ts-ignore
+      style: colors.map(v => COLOR_POS(v)).map(v => ({
+        url: `data:image/svg+xml;base64,${btoa(v)}`,
+        size: new AMap.Size(11, 11),
+        anchor: new AMap.Pixel(5, 5)
+      }))
+    })
 
-  [...document.querySelectorAll("input[type=checkbox]")].forEach((dom: Element) => {
-    (dom as HTMLInputElement).onchange = (e: any) => {
-      console.log(markers[e.target.name])
-      if (!e.target?.checked) {
-        checked.push(e.target?.name)
-        // @ts-ignore
-        layer.remove(markers[e.target.name])
-      } else {
-        checked = checked.filter(v => v !== e.target?.name)
-        // @ts-ignore
-        layer.add(markers[e.target.name])
-      }
-    }
+    massMarks[k].setMap(map)
   })
 
+  const Filter: React.FunctionComponent<{
+    names: typeof names
+    onChange: (action: string, data: string) => any
+  }> = ({ names, onChange }) => {
+    const [selected, setSelected] = useState<string[]>(names.map(v => v.split("-")[0]))
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      console.log(e.target.checked, e.target.name, markers, markers[e.target.name])
+      if (e.target.checked) {
+        setSelected([...selected, e.target.name])
+        onChange('add', e.target.name)
+      } else {
+        setSelected(selected.filter(v => v !== e.target.name))
+        onChange('remove', e.target.name)
+      }
+    }
+
+    return <ul>
+      {names.map(v => {
+        const name = v.split("-")[0]
+        return (
+          <li>
+            <label>
+              <span>{v}</span>
+              <input type="checkbox" onChange={handleChange} checked={selected.includes(name)} name={name} />
+            </label>
+          </li>
+        )
+      })}
+    </ul>
+  }
+
+  ReactDOM.render(<Filter names={names} onChange={(action, data) => {
+    // @ts-ignore
+    layer[action](markers[data])
+    massMarks[data][action === 'add' ? 'show' : 'hide']()
+  }} />, document.getElementById("root"))
 }
 const url = `https://webapi.amap.com/maps?v=1.4.15&key=${import.meta.env.VITE_AMAP_KEY}&callback=amapLoaded`
 var jsapi = document.createElement('script');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import dsv from '@rollup/plugin-dsv';
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default {
     plugins: [
-        dsv()
+        dsv(),
+        reactRefresh()
     ]
 }
